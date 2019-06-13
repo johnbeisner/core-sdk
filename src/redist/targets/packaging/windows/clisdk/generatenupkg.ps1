4b0e2a55dfe7b5f59dc6c1ff24d45c93fbdb87c5
jbeisner <jbeisner@microsoft.com>
2019-06-13 23:55:14 +0000

    Use the Major.Minor version in the NuPkg ID

--- src/redist/targets/packaging/windows/clisdk/generatenupkg.ps1
+++ src/redist/targets/packaging/windows/clisdk/generatenupkg.ps1
@@ -9,7 +9,8 @@ param(
     [Parameter(Mandatory=$false)][string]$Architecture,
     [Parameter(Mandatory=$false)][string]$ComponentName,
     [Parameter(Mandatory=$false)][string]$ComponentFriendlyName,
-    [Parameter(Mandatory=$false)][string]$ProjectUrlSuffix
+    [Parameter(Mandatory=$false)][string]$ProjectUrlSuffix,
+    [Parameter(Mandatory=$false)][string]$MajorMinorVersion
 )
 
 $RepoRoot = Convert-Path "$PSScriptRoot\..\..\..\..\..\.."
@@ -32,5 +33,5 @@ if (Test-Path $NupkgFile) {
     Remove-Item -Force $NupkgFile
 }
 
-& $NuGetExe pack $NuspecFile -Version $NugetVersion -OutputDirectory $OutputDirectory -NoDefaultExcludes -NoPackageAnalysis -Properties DOTNET_BUNDLE=$SdkBundlePath`;ARCH=$Architecture`;COMPONENT_NAME=$ComponentName`;F_NAME=$ComponentFriendlyName`;URL_SUFFIX=$ProjectUrlSuffix
+& $NuGetExe pack $NuspecFile -Version $NugetVersion -OutputDirectory $OutputDirectory -NoDefaultExcludes -NoPackageAnalysis -Properties DOTNET_BUNDLE=$SdkBundlePath`;ARCH=$Architecture`;COMPONENT_NAME=$ComponentName`;F_NAME=$ComponentFriendlyName`;URL_SUFFIX=$ProjectUrlSuffix`;MM_VER=$MajorMinorVersion
 Exit $LastExitCode
